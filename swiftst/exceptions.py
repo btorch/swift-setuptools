e5c01db3385098fd72027f151e30fbc2238ed979
btorch <btorch@gmail.com>
2013-04-05 17:49:10 -0500

    adding config error

--- swiftst/exceptions.py
+++ swiftst/exceptions.py
@@ -22,3 +22,18 @@ class HostListError(Error):
     def __repr__(self):
         return '%d - %s' % (self.status, self.reason)
 
+
+class ConfigFileError(Error):
+    """
+    Raised when host list generation fails.
+    """
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+        Error.__init__(self)
+
+    def __str__(self):
+        return '%d - %s' % (self.status, self.reason)
+
+    def __repr__(self):
+        return '%d - %s' % (self.status, self.reason)
