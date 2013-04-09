5f9fc80b067823e5756c5eae9da9c2b4e63c03e6
btorch <btorch@gmail.com>
2013-04-09 14:53:12 -0500

    adding a generic class

--- swiftst/exceptions.py
+++ swiftst/exceptions.py
@@ -7,6 +7,22 @@ class Error(StandardError):
     pass
 
 
+class ResponseError(Error):
+    """
+    A General response error
+    """
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+        Error.__init__(self)
+
+    def __str__(self):
+        return '%d - %s' % (self.status, self.reason)
+
+    def __repr__(self):
+        return '%d - %s' % (self.status, self.reason)
+
+
 class HostListError(Error):
     """
     Raised when host list generation fails.
