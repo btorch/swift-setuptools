d988d31bda14890061f82c86ea9e1a030b8a8b44
btorch <btorch@gmail.com>
2013-04-09 12:47:47 -0500

    adding new class for templates

--- swiftst/exceptions.py
+++ swiftst/exceptions.py
@@ -37,3 +37,20 @@ class ConfigFileError(Error):
 
     def __repr__(self):
         return '%d - %s' % (self.status, self.reason)
+
+
+class TemplateFileError(Error):
+    """
+    Raised when something goes wrong
+    manipulating a template file.
+    """
+    def __init__(self, status, reason):
+        self.status = status
+        self.reason = reason
+        Error.__init__(self)
+
+    def __str__(self):
+        return '%d - %s' % (self.status, self.reason)
+
+    def __repr__(self):
+        return '%d - %s' % (self.status, self.reason)
