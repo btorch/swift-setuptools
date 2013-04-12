84345e8cfe518047ab7812dca488416823244c1f
btorch <btorch@gmail.com>
2013-04-12 14:24:23 -0500

    modifying functions

--- swiftst/exceptions.py
+++ swiftst/exceptions.py
@@ -24,50 +24,30 @@ class ResponseError(Error):
         return '%d - %s' % (self.status, self.reason)
 
 
-class HostListError(Error):
+class HostListError(ResponseError):
     """
     Raised when host list generation fails.
     """
-    def __init__(self, status, reason):
-        self.status = status
-        self.reason = reason
-        Error.__init__(self)
-
-    def __str__(self):
-        return '%d - %s' % (self.status, self.reason)
-
-    def __repr__(self):
-        return '%d - %s' % (self.status, self.reason)
+    pass
 
 
-class ConfigFileError(Error):
+class ConfigFileError(ResponseError):
     """
     Raised when host list generation fails.
     """
-    def __init__(self, status, reason):
-        self.status = status
-        self.reason = reason
-        Error.__init__(self)
-
-    def __str__(self):
-        return '%d - %s' % (self.status, self.reason)
-
-    def __repr__(self):
-        return '%d - %s' % (self.status, self.reason)
+    pass
 
 
-class TemplateFileError(Error):
+class ConfigSyncError(ResponseError):
+    """
+    Raised when issue syncing configs to system's root
+    """
+    pass
+
+
+class TemplateFileError(ResponseError):
     """
     Raised when something goes wrong
     manipulating a template file.
     """
-    def __init__(self, status, reason):
-        self.status = status
-        self.reason = reason
-        Error.__init__(self)
-
-    def __str__(self):
-        return '%d - %s' % (self.status, self.reason)
-
-    def __repr__(self):
-        return '%d - %s' % (self.status, self.reason)
+    pass
