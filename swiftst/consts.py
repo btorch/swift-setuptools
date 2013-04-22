273ef3efaa977fbeb7fc8cf231258b463c7aa370
btorch <btorch@gmail.com>
2013-04-22 18:33:53 -0500

    adding python-keystone to packages

--- swiftst/consts.py
+++ swiftst/consts.py
@@ -24,7 +24,7 @@ Dictionary that contains system:packages that will be installed
 for swift according to each system functionality
 '''
 packages = {'generic': ['swift', 'python-swift', 'python-swiftclient'],
-            'proxy': ['swift-proxy', 'python-keystoneclient'],
+            'proxy': ['swift-proxy', 'python-keystone', 'python-keystoneclient'],
             'storage': ['swift-account', 'swift-container', 'swift-object'],
             'saio': ['swift-proxy', 'swift-account', 'swift-container',
                      'swift-object'],
